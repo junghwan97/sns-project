134db321a89b225fe649cb6d3461fdd22c107747
junghwan <jhzzang97@gmail.com>
2024-07-14 10:06:45 +0900

    feature : 피드 조회 기능 구현 - 전체글 조회 - 내가 쓴 글 조회

--- src/main/java/sns/snsproject/repository/PostEntityRepository.java
+++ src/main/java/sns/snsproject/repository/PostEntityRepository.java
@@ -1,9 +1,13 @@
 package sns.snsproject.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sns.snsproject.model.entity.PostEntity;
+import sns.snsproject.model.entity.UserEntity;
 
 @Repository
 public interface PostEntityRepository extends JpaRepository<PostEntity, Long> {
+    Page<PostEntity> findAllByUser(UserEntity userEntity, Pageable pageable);
 }
